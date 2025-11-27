79560dfe5c23b36e03427b94d0fed379a12dc921
JasonWeinberger <info@smartclose.ai>
2025-11-26 19:44:31 -0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -21,5 +21,5 @@ export const INDUSTRIES: IndustryOption[] = [
 // 5. Copy the generated link (starts with buy.stripe.com) and paste it below.
 
 export const STRIPE_LINKS = {
-  monthly: 'https://buy.stripe.com/test_28E8wPc2gbNP2a96tb8Vi01', // Test Stripe Link
+  monthly: 'https://buy.stripe.com/8x28wP4zO19b8yx2cV8Vi0F', // Test Stripe Link
 };
